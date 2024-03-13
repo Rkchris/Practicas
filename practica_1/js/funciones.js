b662c630db0bb5b1743ae6bf84cdfca7c46fbe5e
RKchris <christiancs1007@gmail.com}>
2024-03-12 20:04:00 -0600

    Add practice 1

---
+++ practica_1/js/funciones.js
@@ -0,0 +1,6 @@
+// crear una funcion que muestre una alerta con el mensaje "Hola Mundo" al cargar la pagina
+function holaMundo(){
+    alert("Hola Mundo");
+}
+
+holaMundo(); // llamamos a la funcion para que se ejecute al cargar la pagina
